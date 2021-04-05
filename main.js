d5fbc944255c9387ba9fc4d3dcb11b7935f9674e
creatorneemay <72016905+creatorneemay@users.noreply.github.com>
2021-04-05 19:43:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nosex=0
+nosey=0
+leftwristx=0
+rightwristx=0
+diffrence=0
 function preload(){}
 function setup(){
     canvas=createCanvas(400,400);
@@ -13,8 +18,17 @@ function modelloaded(){
 function gotposes(results){
     if(results.length>0){
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        diffrence=floor(leftwristx-rightwristx)
     }
 }
 function draw(){
     background("#f78c2f");
+    stroke("red");
+    fill("blue");
+    square(nosex,nosey,diffrence);
+    document.getElementById("square_size").innerHTML="The size of square = "+diffrence
 }
